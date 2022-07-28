b28e4e01cd31facf9d994a2f412c03da4e32e969
Jean-Paul Lizotte <36302005+jean-paullizotte@users.noreply.github.com>
2022-07-28 16:34:08 -0400

    Update buildall.ps1

--- buildall.ps1
+++ buildall.ps1
@@ -3,9 +3,9 @@
 #dotnet tool install dotnet-ef
 #dotnet tool install --global dotnet-sonarscanner --version 5.4.1
 #nuget restore
-#dotnet sonarscanner begin /key:"GOAT" /name:"GOAT" /d:sonar.host.url="http://20.116.21.79/" /d:sonar.login="d432b0cebbe841766e9e43faa44c7f56fc6ba86c" /version:0.0.0.8 /d:sonar.cs.vscoveragexml.reportsPaths="%temp%\*.coveragexml" /d:sonar.exclusions='**/wwwroot/lib/**'
+#dotnet sonarscanner begin /key:"GOAT" /name:"GOAT" /d:sonar.host.url="http://20.116.21.79/" /d:sonar.login="*************************" /version:0.0.0.8 /d:sonar.cs.vscoveragexml.reportsPaths="%temp%\*.coveragexml" /d:sonar.exclusions='**/wwwroot/lib/**'
 #msbuild .\WebGoat.NET.sln
-#dotnet sonarscanner end /d:sonar.login="d432b0cebbe841766e9e43faa44c7f56fc6ba86c"
+#dotnet sonarscanner end /d:sonar.login="***************************"
 Clear-Host
 set-alias -name codeql -value "C:\work\code\tools\codeql\codeql.exe"
 $db = "C:\temp\codeql_database"
